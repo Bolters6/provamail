5f52e96fb1ba162f1b319b47f7f38368e2489b0d
Bolters6 <mariopontillo6@gmail.com>
2022-03-23 15:39:53 +0100

    prova mail

---
+++ src/main/java/com/example/provamail/EmailController.java
@@ -0,0 +1,21 @@
+package com.example.provamail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/mail")
+public class EmailController {
+
+    @Autowired
+    private EmailServiceImpl emailService;
+
+    @PostMapping(path = "/send")
+    public ResponseEntity<Void> sendEmail(){
+        emailService.sendSimpleMessage("Job-description@dstech.it", "prova", "funciona");
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/test/java/com/example/provamail/ProvamailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provamail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvamailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
